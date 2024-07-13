9ebc1d5700e1fcc7b1127a003f9773c2414f6d71
hly <1611767083@qq.com>
2024-07-14 00:02:08 +0800

    paper-detail菜单栏和底栏修改为新ui 设置headtitle

--- paper/menu.js
+++ paper/menu.js
@@ -3,3 +3,13 @@ function toggleMenu() {
     var navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
+
+// 监测向下滑动
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
